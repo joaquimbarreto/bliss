22ddc799dcd88789c6763aca57fe1557eb2f22c5
Joaquim Barreto <joaquim@barreto.co>
2019-07-10 18:43:56 +0100

    wednesday end

---
+++ src/components/DetailScreen.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DetailScreen = () => {
+  return (
+    <div>
+      <button>Hello</button>
+    </div>
+  );
+};
+
+export default DetailScreen;
--- src/components/Share.js
+++ src/components/Share.js
@@ -1,11 +1,61 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-function Share() {
+const shareAPI =
+  "https://private-bbbe9-blissrecruitmentapi.apiary-mock.com/share";
+
+const Share = () => {
+  const [sharing, setSharing] = useState(false);
+  const [email, setEmail] = useState("");
+  const [url, setUrl] = useState("");
+
+  useEffect(() => {
+    setUrl(window.location.href);
+  }, []);
+
+  const handleClick = () => {
+    setSharing(!sharing);
+  };
+
+  const handleSubmit = () => {
+    fetch(shareAPI, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        destination_email: email,
+        content_url: url
+      })
+    });
+  };
+
+  const handleChange = event => {
+    setEmail(event.target.value);
+  };
+
   return (
     <div>
-      <button>Share</button>
+      <button onClick={handleClick}>Share</button>
+      {sharing && (
+        <form action="" method="get" onSubmit={handleSubmit}>
+          <div className="input">
+            <input
+              type="text"
+              name="email"
+              id="email"
+              placeholder="Your Email"
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="form-example">
+            <input type="submit" value="Send!" />
+          </div>
+        </form>
+      )}
     </div>
   );
-}
+};
 
 export default Share;
